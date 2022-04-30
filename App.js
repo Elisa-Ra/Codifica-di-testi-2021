43139229cdaf4c8d571d3248330e2ea576c62e28
Elisa-Ra <elyraptor22@gmail.com>
2022-04-30 17:33:33 +0200

    Nuova versione

--- App.js
+++ App.js
@@ -1,51 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableHighlight, Linking, Image } from 'react-native';
-import {
-  responsiveHeight,
-  responsiveWidth,
-  responsiveFontSize,
-  responsiveScreenHeight,
-  responsiveScreenWidth,
-  responsiveScreenFontSize
-} from "react-native-responsive-dimensions";
+import { WebView } from 'react-native-webview';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+
+const Stack = createStackNavigator();
+
+function ScreenA({navigation}){ //pagina iniziale
+  const screenb= () => {
+    navigation.navigate('Ristostore');
+  }
+  const screenc= () => {
+    navigation.navigate('Area Drivers');
+  }
+  const screend= () => {
+    navigation.navigate('Area Gestori');
+  }
+    return (
+      <View style={styles.container}>
+    
+        
+        <View style={styles.div}>
+          <TouchableWithoutFeedback onPress={screenb}>
+            <Image source={require('./assets/entra.jpg')}  style={styles.img}/>
+         </TouchableWithoutFeedback>
+         
+        </View>
+     <Text style={styles.text}>Prova la nostra app</Text>
+        <View style={styles.div}>
+          <TouchableWithoutFeedback onPress={screenc}>
+            <Image source={require('./assets/rider.jpg')}  style={styles.img}/>
+          </TouchableWithoutFeedback> 
+          
+        </View>
+        <Text style={styles.text}>Area Driver</Text>
+        <View style={styles.div}>
+          <TouchableWithoutFeedback onPress={screend}>
+            <Image source={require('./assets/gestori.jpg')}  style={styles.img}/>
+          </TouchableWithoutFeedback> 
+         
+        </View>
+     <Text style={styles.text}>Area Gestori</Text>
+        <StatusBar style="auto" />
+      </View>
+);
+}
+function ScreenB(){ //ristostore home
+  return (
+   
+      <WebView name='1'
+        source={{ uri: 'https://ristostore.it/' }} 
+      />
+  
+  );
+}
+function ScreenC(){ //ristostore driver
+  return (
+    
+      <WebView name='2'
+        source={{ uri: 'https://ristostore.it/Area_Drivers/accesso' }} 
+      />
+   
+  );
+}
+function ScreenD(){ //ristostore gestori
+  return (
+    
+      <WebView name='3'
+        source={{ uri: 'https://ristostore.it/Area_Gestori/accesso' }} 
+      />
+    
+  );
+}
 
 export default function App() {
-  const home=()=>{Linking.openURL('https://ristostore.it/')}
-  const gestori=()=>{Linking.openURL('https://ristostore.it/Area_Gestori/accesso')}
-  const driver=()=>{Linking.openURL('https://ristostore.it/Area_Drivers/accesso')}
-  
-  return (
-    <View style={styles.container}>
-      
-        <Text style={styles.text1}>Ristostore</Text>
-        <View style={styles.div}>
-          <TouchableHighlight onPress={home}>
-            
-            <Image source={require('./assets/entra.jpg')}  style={styles.home}/>
-            
-          </TouchableHighlight>
-          <Text style={styles.text}>Prova la nostra app</Text>
-        </View>
-       
-        <View style={styles.div}>
-          <TouchableHighlight onPress={driver}>
-          <Image source={require('./assets/riders.jpg')}  style={styles.riders}/>
-         
-          </TouchableHighlight> 
-          <Text style={styles.text}>Area Driver</Text>
-        </View>
-        <View style={styles.div}>
-          <TouchableHighlight onPress={gestori}>
-          <Image source={require('./assets/gestori.jpg')}  style={styles.gestori}/>
-         
-          </TouchableHighlight> 
-          <Text style={styles.text}>Area Gestori</Text>
-        </View>
-      
-
-
-      <StatusBar style="auto" />
-    </View>
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Ristostore App" component = {ScreenA}/>
+          <Stack.Screen name="Ristostore" component = {ScreenB} />
+          <Stack.Screen name="Area Drivers" component = {ScreenC}/>
+          <Stack.Screen name="Area Gestori" component = {ScreenD}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -59,39 +100,26 @@ const styles = StyleSheet.create({
   div:{
     flex:1,
     width:'100%',
-    margin:'10%',
-    
-    
+    height:'100%',
   },
-  home:{
-    resizeMode: 'cover',
+  img:{
+    resizeMode: 'contain',
     width: '100%',
-    height: '90%'
-
+    height: '100%',
+    
   }, 
-  gestori:{
-    resizeMode: 'cover',
-    width: '100%',
-    height: '90%'
-
-  },
-  riders:{
-    resizeMode: 'cover',
-    width: '100%',
-    height: '90%'
-
-  },
   text:{
     textAlign:'center',
-    color:'#008b8b',
-    
+    color:'#17a2b8',
+    fontFamily:'serif',
     fontSize:20
   },
   text1:{
     textAlign:'center',
-    color:'#008b8b',
-    margin:'5%',
-    fontSize:30
+    color:'#17a2b8',
+    fontSize:20,
+    fontFamily:'serif'
   }
 
 });
+
